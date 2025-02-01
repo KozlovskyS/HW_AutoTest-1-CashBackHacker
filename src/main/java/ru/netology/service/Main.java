7f3d80702bd094e08ef7912005b32012d9cbb8ba
KozlovskyS <skozmail@gmail.com>
2025-02-01 19:41:59 +0300

    added test testng

--- src/main/java/ru/netology/service/Main.java
+++ src/main/java/ru/netology/service/Main.java
@@ -3,7 +3,5 @@ package ru.netology.service;
 
 public class Main {
     public static void main(String[] args) {
-
-        }
     }
 }
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void testIfLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testIfIdenticalBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testIfLessBoundaryMin() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testIfLessBoundaryMax() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_001;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+}
+
