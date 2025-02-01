1b9e7de142f892fd8d33de335c128fb003d33aa3
KozlovskyS <skozmail@gmail.com>
2025-02-01 22:05:58 +0300

    added test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,59 +1,61 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfLessBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int expected = 100;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfIdenticalBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1_000;
 
         int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfIdenticalMultipleBoundary() {
         CashbackHackService service = new CashbackHackService();
         int amount = 2_000;
 
         int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfLessBoundaryMin() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
         int expected = 1;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfLessBoundaryMax() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1_001;
 
         int expected = 999;
         int actual = service.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testIfNegativeAmount() {
         CashbackHackService service = new CashbackHackService();
         int amount = -150;
